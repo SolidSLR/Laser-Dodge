63a1681a45dd493bb2a2543fc8466ebf55b886ee
ricfdzgon <ricfdzgon@gmail.com>
2021-12-24 16:48:01 +0100

    Movimiento hacia la izquierda #3

--- Assets/Scripts/Nave.cs
+++ Assets/Scripts/Nave.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Nave : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float speed = 2F;
+    private float actualspeed;
+    Animator animator;
+    private Rigidbody2D rb;
+    private Collider2D collider;
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.right * -speed * Time.deltaTime);
+        }
     }
 }
