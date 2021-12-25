2b54c2ec9fc520e63e8886e874317c83853450b3
Santiago López Rey <devsantilopez@gmail.com>
2021-12-25 14:35:24 +0100

    Añadida rutina que controla el spawn de los láseres #4

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SceneController : MonoBehaviour
 {
-
+    public GameObject laserPrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +16,34 @@ public class SceneController : MonoBehaviour
     void Update()
     {
 
+    }
+
+    private IEnumerator corutinaSpawn(){
+        int x=0;
+        int random;
+        while(x < 20){
+            random = Random.Range(1,5);
+            
+            yield return new WaitForSeconds(1.5f);
+            if(random==1){
+                spawn(laserPrefab, laserPrefab.GetComponent<Laser>().RightSpawnPoint);
+                Debug.Log("Derecha");
+                x++;
+            } else if(random==2){
+                spawn(laserPrefab, laserPrefab.GetComponent<Laser>().LeftSpawnPoint);
+                Debug.Log("Izquierda");
+                x++;
+            }else if(random==3){
+                spawn(laserPrefab, laserPrefab.GetComponent<Laser>().TopSpawnPoint);
+                Debug.Log("Arriba");
+                x++;
+            } else if(random==4){
+                spawn(laserPrefab, laserPrefab.GetComponent<Laser>().BotSpawnPoint);
+                Debug.Log("Abajo");
+                x++;
+            }
+        }
+        
     }
 
 }
