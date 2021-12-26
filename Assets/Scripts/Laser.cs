d7ad29e8645758a089a1c2ae7112b04427f31abc
Santiago López Rey <devsantilopez@gmail.com>
2021-12-26 18:10:23 +0100

    Añadidos puntos de spawn restantes

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -7,31 +7,79 @@ public class Laser : MonoBehaviour
 
 private float speed =20f;
 
+    //Puntos de spawn por la izquierda
+    private Vector3 leftSpawnPoint1 = new Vector3(-22.99f, -1.64f, 0f);
 
-    private Vector3 leftSpawnPoint = new Vector3(-20f, 0f, 0f);
-    //private Vector3 leftSpawnPoint = new Vector3(spawnPoint.transform.position.x, spawnPoint.transform.position.y, 0);
+    private Vector3 leftSpawnPoint2 = new Vector3(-22.99f, 8.61f, 0f);
 
-    private Vector3 rightSpawnPoint = new Vector3(20f, 0f, 0f);
+    private Vector3 leftSpawnPoint3 = new Vector3(-22.99f, -11.54f, 0f);
 
-    private Vector3 topSpawnPoint = new Vector3(0f, 20f, 0f);
+    //Puntos de spanw por la derecha
+    private Vector3 rightSpawnPoint1 = new Vector3(22.99f, -1.64f, 0f);
 
-    private Vector3 botSpawnPoint = new Vector3(0f, -20f, 0f);
+    private Vector3 rightSpawnPoint2 = new Vector3(22.99f, 8.61f, 0f);
 
-    public Vector3 LeftSpawnPoint{
-        get { return leftSpawnPoint;}
+    private Vector3 rightSpawnPoint3 = new Vector3(22.99f, -11.54f, 0f);
+
+    //Puntos de spwan por arriba
+    private Vector3 topSpawnPoint1 = new Vector3(0f, 20f, 0f);
+
+    private Vector3 topSpawnPoint2 = new Vector3(-15f, 20f, 0f);
+
+    private Vector3 topSpawnPoint = new Vector3(15f, 20f, 0f);
+
+    //Puntos de spawn por abajo
+    private Vector3 botSpawnPoint1 = new Vector3(0f, -20f, 0f);
+
+    private Vector3 botSpawnPoint2 = new Vector3(0f, -20f, 0f);
+
+    private Vector3 botSpawnPoint3 = new Vector3(0f, -20f, 0f);
+    public Vector3 LeftSpawnPoint1{
+        get { return leftSpawnPoint1;}
     }
 
-    public Vector3 RightSpawnPoint{
-        get { return rightSpawnPoint;}
+    public Vector3 LeftSpawnPoint2{
+        get { return leftSpawnPoint2;}
     }
 
-    public Vector3 BotSpawnPoint{
-        get { return botSpawnPoint;}
+    public Vector3 LeftSpawnPoint3{
+        get { return leftSpawnPoint3;}
+    }
+    public Vector3 RightSpawnPoint1{
+        get { return rightSpawnPoint1;}
     }
 
-    public Vector3 TopSpawnPoint{
-        get { return topSpawnPoint;}
+    public Vector3 RightSpawnPoint2{
+        get { return rightSpawnPoint2;}
     }
+
+    public Vector3 RightSpawnPoint3{
+        get { return rightSpawnPoint3;}
+    }
+
+    public Vector3 BotSpawnPoint1{
+        get { return botSpawnPoint1;}
+    }
+
+    public Vector3 BotSpawnPoint2{
+        get { return botSpawnPoint2;}
+    }
+
+    public Vector3 BotSpawnPoint3{
+        get { return botSpawnPoint3;}
+    }
+    public Vector3 TopSpawnPoint1{
+        get { return topSpawnPoint1;}
+    }
+
+    public Vector3 TopSpawnPoint2{
+        get { return topSpawnPoint2;}
+    }
+
+    public Vector3 TopSpawnPoint3{
+        get { return topSpawnPoint3;}
+    }
+
     // Start is called before the first frame update
     void Start()
     {
