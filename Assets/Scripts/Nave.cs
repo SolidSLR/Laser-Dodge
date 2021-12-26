41bcd837d5d611d9141d3177a384d13131c24dba
ricfdzgon <ricfdzgon@gmail.com>
2021-12-26 04:32:54 +0100

    Rotación del sprite en el eje de Y para las flechas arriba abajo #3

--- Assets/Scripts/Nave.cs
+++ Assets/Scripts/Nave.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Nave : MonoBehaviour
 {
+    //Declaración de variables
     private float speed = 5F;
     Animator animator;
     private Rigidbody2D rb;
@@ -17,9 +18,14 @@ public class Nave : MonoBehaviour
 
     void Update()
     {
+
+        //Movimiento básico de la nave
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Translate(Vector3.right * -speed * Time.deltaTime);
+            //  transform.Rotate(0, 0, -90);
+            //Esto puede ser una idea para rotarlo en el eje de la z, el problema
+            // es que lo hace en cada frame, hay que conseguir que solo se haga una vez
         }
 
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
@@ -30,11 +36,13 @@ public class Nave : MonoBehaviour
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
             transform.Translate(Vector3.up * speed * Time.deltaTime);
+            GetComponent<SpriteRenderer>().flipY = false;
         }
 
         if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
             transform.Translate(Vector3.down * speed * Time.deltaTime);
+            GetComponent<SpriteRenderer>().flipY = true;
         }
     }
 }
