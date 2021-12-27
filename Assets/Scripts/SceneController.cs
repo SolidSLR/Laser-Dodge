3090d0a2585e9889861da31f0c12057a591af4aa
Santiago López Rey <devsantilopez@gmail.com>
2021-12-27 18:40:39 +0100

    Añadidos de nuevo scripts de laser y control de escena

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -14,6 +14,7 @@ public class SceneController : MonoBehaviour
         }
 
         StartCoroutine("corutinaSpawn");
+        //Debug.Log("Jelouda");
     }
 
     // Update is called once per frame
