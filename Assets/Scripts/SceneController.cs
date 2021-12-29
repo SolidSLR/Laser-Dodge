e9c99da8a0d58f9497720e943ec1179b13dd70f6
Ricardo Fernández <ricfdzgon@gmail.com>
2021-12-29 22:07:08 +0100

    Llamada desde el SceneController #23

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class SceneController : MonoBehaviour
 {
     public GameObject laserPrefab;
-
     // Start is called before the first frame update
     void Start()
     {
-        if(laserPrefab==null){
+        if (laserPrefab == null)
+        {
             Debug.Log("SceneController: Ay caramba, no hay balas para tí (no se ha establecido el prefab)");
         }
 
@@ -20,70 +20,97 @@ public class SceneController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        Nave nave = new Nave();
+        nave.getGameOver();
     }
 
-    private IEnumerator corutinaSpawn(){
-        int x=0;
+    private IEnumerator corutinaSpawn()
+    {
+        int x = 0;
         int random;
-        while(x < 50){
-            random = Random.Range(1,13);
-            
+        while (x < 50)
+        {
+            random = Random.Range(1, 13);
+
             yield return new WaitForSeconds(1.5f);
-            if(random==1){
+            if (random == 1)
+            {
                 spawn(laserPrefab, laserPrefab.GetComponent<Laser>().RightSpawnPoint1);
                 Debug.Log("Derecha");
                 x++;
-            } else if(random==2){
+            }
+            else if (random == 2)
+            {
                 spawn(laserPrefab, laserPrefab.GetComponent<Laser>().RightSpawnPoint2);
                 Debug.Log("Izquierda");
                 x++;
-            }else if(random==3){
+            }
+            else if (random == 3)
+            {
                 spawn(laserPrefab, laserPrefab.GetComponent<Laser>().RightSpawnPoint3);
                 Debug.Log("Arriba");
                 x++;
-            } else if(random==4){
+            }
+            else if (random == 4)
+            {
                 spawn(laserPrefab, laserPrefab.GetComponent<Laser>().LeftSpawnPoint1);
                 Debug.Log("Abajo");
                 x++;
-            } else if(random==5){
+            }
+            else if (random == 5)
+            {
                 spawn(laserPrefab, laserPrefab.GetComponent<Laser>().LeftSpawnPoint2);
                 Debug.Log("Abajo");
                 x++;
-            } else if(random==6){
+            }
+            else if (random == 6)
+            {
                 spawn(laserPrefab, laserPrefab.GetComponent<Laser>().LeftSpawnPoint3);
                 Debug.Log("Abajo");
                 x++;
-            } else if(random==7){
+            }
+            else if (random == 7)
+            {
                 spawn(laserPrefab, laserPrefab.GetComponent<Laser>().TopSpawnPoint1);
                 Debug.Log("Abajo");
                 x++;
-            } else if(random==8){
+            }
+            else if (random == 8)
+            {
                 spawn(laserPrefab, laserPrefab.GetComponent<Laser>().TopSpawnPoint2);
                 Debug.Log("Abajo");
                 x++;
-            } else if(random==9){
+            }
+            else if (random == 9)
+            {
                 spawn(laserPrefab, laserPrefab.GetComponent<Laser>().TopSpawnPoint3);
                 Debug.Log("Abajo");
                 x++;
-            } else if(random==10){
+            }
+            else if (random == 10)
+            {
                 spawn(laserPrefab, laserPrefab.GetComponent<Laser>().BotSpawnPoint1);
                 Debug.Log("Abajo");
                 x++;
-            } else if(random==11){
+            }
+            else if (random == 11)
+            {
                 spawn(laserPrefab, laserPrefab.GetComponent<Laser>().BotSpawnPoint2);
                 Debug.Log("Abajo");
                 x++;
-            } else if(random==12){
+            }
+            else if (random == 12)
+            {
                 spawn(laserPrefab, laserPrefab.GetComponent<Laser>().BotSpawnPoint3);
                 Debug.Log("Abajo");
                 x++;
             }
         }
-        
+
     }
 
-    private void spawn(GameObject prefab, Vector3 spawnPoint){
+    private void spawn(GameObject prefab, Vector3 spawnPoint)
+    {
 
         var go = Instantiate(prefab, spawnPoint, Quaternion.identity);
         //go.transform.
