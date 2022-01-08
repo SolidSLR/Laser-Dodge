e8a8bf0a2621bc5445f12e50bbe7c1ab2c0a5da9
Santiago López Rey <devsantilopez@gmail.com>
2022-01-08 12:22:18 +0100

    Corregidos spawnpoints, posición de vidas y ejecución de sonidos

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -11,32 +11,32 @@ public class Laser : MonoBehaviour
     private Collider2D colision;
 
     //Puntos de spawn por la izquierda
-    private Vector3 leftSpawnPoint1 = new Vector3(-22.99f, -1.64f, 0f);
+    private Vector3 leftSpawnPoint1 = new Vector3(16.01f, -21.76f, 0f);
 
-    private Vector3 leftSpawnPoint2 = new Vector3(-22.99f, 8.61f, 0f);
+    private Vector3 leftSpawnPoint2 = new Vector3(-6.87f, -21.76f, 0f);
 
-    private Vector3 leftSpawnPoint3 = new Vector3(-22.99f, -11.54f, 0f);
+    private Vector3 leftSpawnPoint3 = new Vector3(4.23f, -21.67f, 0f);
 
     //Puntos de spanw por la derecha
-    private Vector3 rightSpawnPoint1 = new Vector3(22.99f, -1.64f, 0f);
+    private Vector3 rightSpawnPoint1 = new Vector3(24.9f, -12.42f, 0f);
 
-    private Vector3 rightSpawnPoint2 = new Vector3(22.99f, 8.61f, 0f);
+    private Vector3 rightSpawnPoint2 = new Vector3(22.79f, 12.02f, 0f);
 
-    private Vector3 rightSpawnPoint3 = new Vector3(22.99f, -11.54f, 0f);
+    private Vector3 rightSpawnPoint3 = new Vector3(24.96f, 0f, 0f);
 
     //Puntos de spwan por arriba
-    private Vector3 topSpawnPoint1 = new Vector3(0f, 20f, 0f);
+    private Vector3 topSpawnPoint1 = new Vector3(-16.25f, -12.35f, 0f);
 
-    private Vector3 topSpawnPoint2 = new Vector3(-15f, 20f, 0f);
+    private Vector3 topSpawnPoint2 = new Vector3(-16.11f, 13.08f, 0f);
 
-    private Vector3 topSpawnPoint3 = new Vector3(15f, 20f, 0f);
+    private Vector3 topSpawnPoint3 = new Vector3(-16.84f, 0f, 0f);
 
     //Puntos de spawn por abajo
-    private Vector3 botSpawnPoint1 = new Vector3(0f, -20f, 0f);
+    private Vector3 botSpawnPoint1 = new Vector3(15.32f, 20f, 0f);
 
-    private Vector3 botSpawnPoint2 = new Vector3(0f, -20f, 0f);
+    private Vector3 botSpawnPoint2 = new Vector3(-8.39f, 20f, 0f);
 
-    private Vector3 botSpawnPoint3 = new Vector3(0f, -20f, 0f);
+    private Vector3 botSpawnPoint3 = new Vector3(4.69f, 20f, 0f);
     public Vector3 LeftSpawnPoint1{
         get { return leftSpawnPoint1;}
     }
@@ -111,9 +111,9 @@ public class Laser : MonoBehaviour
     void Update()
     {
        if(transform.position.x>0||transform.position.x<0){
-            transform.position = new Vector3(transform.position.x + speed*Time.deltaTime, transform.position.y, -2);
+            transform.position = new Vector3(transform.position.x + speed*Time.deltaTime, transform.position.y, -5);
         }else {
-            transform.position = new Vector3(transform.position.x, transform.position.y + speed*Time.deltaTime, -2);
+            transform.position = new Vector3(transform.position.x, transform.position.y + speed*Time.deltaTime, -5);
         }
 
         BulletHit();
