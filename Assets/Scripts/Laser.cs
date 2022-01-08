35ac28bf78df96a4d1d6f7a4dcc0a2f1bd38bfeb
Santiago López Rey <devsantilopez@gmail.com>
2022-01-08 12:56:45 +0100

    Reajuste de puntos de spawn y modificación de volumen

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -4,39 +4,38 @@ using UnityEngine;
 
 public class Laser : MonoBehaviour
 {
-    //Velocidad de la bala
-    private float speed =25f;
 
-    //Collider de la bala
+private float speed =25f;
+
     private Collider2D colision;
 
     //Puntos de spawn por la izquierda
-    private Vector3 leftSpawnPoint1 = new Vector3(16.01f, -16f, 0f);
+    private Vector3 leftSpawnPoint1 = new Vector3(-22.99f, -1.64f, 0f);
 
-    private Vector3 leftSpawnPoint2 = new Vector3(-6.87f, -16f, 0f);
+    private Vector3 leftSpawnPoint2 = new Vector3(-22.99f, 8.61f, 0f);
 
-    private Vector3 leftSpawnPoint3 = new Vector3(4.23f, -16f, 0f);
+    private Vector3 leftSpawnPoint3 = new Vector3(-22.99f, -11.54f, 0f);
 
     //Puntos de spanw por la derecha
-    private Vector3 rightSpawnPoint1 = new Vector3(24.9f, -8f, 0f);
+    private Vector3 rightSpawnPoint1 = new Vector3(22.99f, -1.64f, 0f);
 
-    private Vector3 rightSpawnPoint2 = new Vector3(22.79f, 8f, 0f);
+    private Vector3 rightSpawnPoint2 = new Vector3(22.99f, 8.61f, 0f);
 
-    private Vector3 rightSpawnPoint3 = new Vector3(24.96f, 0f, 0f);
+    private Vector3 rightSpawnPoint3 = new Vector3(22.99f, -11.54f, 0f);
 
     //Puntos de spwan por arriba
-    private Vector3 topSpawnPoint1 = new Vector3(-16.25f, -12.35f, 0f);
+    private Vector3 topSpawnPoint1 = new Vector3(0f, 20f, 0f);
 
-    private Vector3 topSpawnPoint2 = new Vector3(-16.11f, 13.08f, 0f);
+    private Vector3 topSpawnPoint2 = new Vector3(-15f, 20f, 0f);
 
-    private Vector3 topSpawnPoint3 = new Vector3(-16.84f, 0f, 0f);
+    private Vector3 topSpawnPoint3 = new Vector3(15f, 20f, 0f);
 
     //Puntos de spawn por abajo
-    private Vector3 botSpawnPoint1 = new Vector3(15.32f, 14f, 0f);
+    private Vector3 botSpawnPoint1 = new Vector3(0f, -20f, 0f);
 
-    private Vector3 botSpawnPoint2 = new Vector3(-8.39f, 14f, 0f);
+    private Vector3 botSpawnPoint2 = new Vector3(0f, -20f, 0f);
 
-    private Vector3 botSpawnPoint3 = new Vector3(4.69f, 14f, 0f);
+    private Vector3 botSpawnPoint3 = new Vector3(0f, -20f, 0f);
     public Vector3 LeftSpawnPoint1{
         get { return leftSpawnPoint1;}
     }
@@ -86,10 +85,6 @@ public class Laser : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
-        colision = GetComponent<Collider2D>();
-        
-
         if(transform.position.x<0){
            speed = Mathf.Abs(speed);
            transform.eulerAngles = new Vector3(0,0,-90);
@@ -100,8 +95,8 @@ public class Laser : MonoBehaviour
            //transform.localScale = new Vector3(1,1,1);
        }else if(transform.position.y<0){
            speed = Mathf.Abs(speed);
-           transform.eulerAngles = new Vector3(0,0,0);
-       }else if(transform.position.y>0 && transform.position.x<0){
+           transform.eulerAngles = new Vector3(0,0,-0);
+       }else if(transform.position.y>0){
            speed = Mathf.Abs(speed)*-1;
            transform.eulerAngles = new Vector3(0,0,180);
            //Debug.Log("Jelouda");
@@ -111,14 +106,14 @@ public class Laser : MonoBehaviour
     void Update()
     {
        if(transform.position.x>0||transform.position.x<0){
-            transform.position = new Vector3(transform.position.x + speed*Time.deltaTime, transform.position.y, -5);
+            transform.position = new Vector3(transform.position.x + speed*Time.deltaTime, transform.position.y, transform.position.z);
         }else {
-            transform.position = new Vector3(transform.position.x, transform.position.y + speed*Time.deltaTime, -5);
+            transform.position = new Vector3(transform.position.x, transform.position.y + speed*Time.deltaTime, transform.position.z);
         }
 
         BulletHit();
- 
     }
+
 
     private void BulletHit(){
         LayerMask mask = LayerMask.GetMask("NaveJugador");
